184621372e3023e946d0d68a6e85461b3d3ba2d2
Taras Yagniuk <mrtaryk@gmail.com>
2012-11-06 02:28:36 +0200

    `examples` schema and relations

---
+++ lib/Dict/Learn/Main/Result/Examples.pm
@@ -0,0 +1,13 @@
+package Dict::Learn::Main::Result::Examples 0.2;
+use base qw[ DBIx::Class::Core ];
+__PACKAGE__->table('example_xref');
+__PACKAGE__->add_columns(qw[ example1_id example2_id dictionary_id
+                             note cdate mdate ]);
+__PACKAGE__->set_primary_key(qw[ example1_id example2_id ]);
+__PACKAGE__->belongs_to( example1_id  => 'Dict::Learn::Main::Result::Example',
+                       { 'foreign.example_id' => 'self.example1_id' });
+__PACKAGE__->belongs_to( example2_id  => 'Dict::Learn::Main::Result::Example',
+                       { 'foreign.example_id' => 'self.example2_id' });
+__PACKAGE__->has_one( dictionary => 'Dict::Learn::Main::Result::Dictionary',
+                    { 'foreign.dictionary_id' => 'self.dictionary_id' });
+1;
