ce2bd1601776e8ad5ef3a32394c341c9e02c8227
Taras Yagniuk <mrtaryk@gmail.com>
2012-11-06 02:27:31 +0200

    changes in `word` schema and relations

--- lib/Dict/Learn/Main/Result/Word.pm
+++ lib/Dict/Learn/Main/Result/Word.pm
@@ -1,13 +1,18 @@
-package Dict::Learn::Main::Result::Word 0.1;
+package Dict::Learn::Main::Result::Word 0.2;
 use base qw[ DBIx::Class::Core ];
 __PACKAGE__->table('word');
-__PACKAGE__->add_columns(qw[ word_id word_orig word_tr dictionary_id
-                             wordclass_id note cdate mdate ]);
+__PACKAGE__->add_columns(qw[ word_id word lang_id wordclass_id
+                             note cdate mdate ]);
 __PACKAGE__->set_primary_key('word_id');
-__PACKAGE__->has_one( dictionary    => 'Dict::Learn::Main::Result::Dictionary', 'dictionary_id');
 __PACKAGE__->has_one( wordclass     => 'Dict::Learn::Main::Result::Wordclass',
-                     { 'foreign.wordclass_id' => 'self.wordclass_id' });
-__PACKAGE__->has_many( word_example => 'Dict::Learn::Main::Result::WordExample', 'word_id');
-__PACKAGE__->many_to_many( examples => 'word_example', 'example_id');
+                    { 'foreign.wordclass_id' => 'self.wordclass_id' });
+
+__PACKAGE__->has_many( examples => 'Dict::Learn::Main::Result::WordExample', 'word_id');
+
+__PACKAGE__->has_many( rel_words => 'Dict::Learn::Main::Result::Words',
+                       { "foreign.word1_id" => "self.word_id" });
+__PACKAGE__->many_to_many( words => 'rel_words', 'word2_id');
+
+# __PACKAGE__->has_many( words_tr  => 'Dict::Learn::Main::Result::Words', 'word2_id');
 
 1;
