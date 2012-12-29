37b16483781f3309bf60061e156b40449968b425
Taras Yagniuk <mrtaryk@gmail.com>
2012-12-29 02:48:32 +0200

    use resultset's `export_data`

--- lib/Dict/Learn/Export.pm
+++ lib/Dict/Learn/Export.pm
@@ -10,6 +10,15 @@ use Data::Printer;
 
 use constant EXPORT_FILENAME => 'export.%d.json';
 
+use constant {
+  TABLE_MAP => { words               => 'Word',
+                 words_xref          => 'Words',
+                 examples            => 'Example',
+                 examples_xref       => 'Examples',
+                 words_examples_xref => 'WordExample',
+  }
+};
+
 sub new {
   my $class = shift;
   my $self  = bless { } => $class;
@@ -20,13 +29,11 @@ sub new {
 sub do {
   my ($self, $filename) = @_;
   my $db = $main::ioc->lookup('db');
-  my $export = encode_json({
-    words               => $db->select_all_words(),
-    words_xref          => $db->select_all_words_xref(),
-    examples            => $db->select_all_examples(),
-    examples_xref       => $db->select_all_examples_xref(),
-    words_examples_xref => $db->select_all_words_examples_xref(),
-  });
+  my $data = {};
+  while ( my ($json_key, $rset_name) = each %{ +TABLE_MAP } ) {
+    $data->{$json_key} = [ $db->schema->resultset($rset_name)->export_data() ];
+  }
+  my $export = encode_json($data);
   $filename //= sprintf(EXPORT_FILENAME, strftime('%Y%m%d%H%M%S', localtime));
   if (my $fh = IO::File->new('> '.$filename))
   {
