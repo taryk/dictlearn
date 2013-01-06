81144b691b4bc7bc666ae75ced8bc917506d8570
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-06 04:48:08 +0200

    Dict::Learn::Main::Result::TestSession: add relationship to `Dict::Learn::Main::Result::TestSessionData`

--- lib/Dict/Learn/Main/Result/TestSession.pm
+++ lib/Dict/Learn/Main/Result/TestSession.pm
@@ -7,5 +7,8 @@ __PACKAGE__->set_primary_key('test_session_id');
 __PACKAGE__->belongs_to( test_id => 'Dict::Learn::Main::Result::Test', 'test_id',
                        { cascade_delete => 0 ,
                          cascade_update => 0 });
-
+__PACKAGE__->has_many( data => 'Dict::Learn::Main::Result::TestSessionData',
+                     { 'foreign.test_session_id' => 'self.test_session_id' },
+                     { cascade_delete => 0 ,
+                       cascade_update => 0 });
 1;
