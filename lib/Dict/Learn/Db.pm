d0aaa77344e2f1d582b62e14b2b6c41212f5ef66
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-13 10:59:12 +0200

    DB: check if required tables exist

--- lib/Dict/Learn/Db.pm
+++ lib/Dict/Learn/Db.pm
@@ -5,14 +5,31 @@ use namespace::autoclean;
 
 use Data::Printer;
 
+use constant REQ_TABLES =>
+  [qw| word word_xref example example_xref word_example_xref
+       dictionary wordclass language test test_session test_session_data
+     |];
+
 use Class::XSAccessor
     accessors => [ qw| schema | ];
 
 sub new {
-  my $class  = shift;
+  my $class = shift;
   my $self = bless {} => $class;
   $self->schema( shift );
   $self
+}
+
+sub check_tables {
+  my $self = shift;
+  say "Checking DB...";
+  my @tables = grep { $_->[0] eq 'main' }
+    map { [ (/^\"(\w+)\"\.\"(\w+)\"$/) ] }
+    $self->schema->storage->dbh->tables();
+  for my $req_table (@{ +REQ_TABLES }) {
+    return unless grep { $req_table eq $_->[1]  } @tables;
+  }
+  1
 }
 
 1;
