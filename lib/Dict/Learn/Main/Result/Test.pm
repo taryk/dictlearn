9541779380d4efb5eb051930bb6f600441dd74f4
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-13 23:24:50 +0200

    `Test` schema: fix column name

--- lib/Dict/Learn/Main/Result/Test.pm
+++ lib/Dict/Learn/Main/Result/Test.pm
@@ -1,7 +1,7 @@
 package Dict::Learn::Main::Result::Test 0.1;
 use base qw[ DBIx::Class::Core ];
 __PACKAGE__->table('test');
-__PACKAGE__->add_columns(qw[ test_id name lang_id enable cdate mdate ]);
+__PACKAGE__->add_columns(qw[ test_id name lang_id enabled cdate mdate ]);
 __PACKAGE__->set_primary_key('test_id');
 __PACKAGE__->has_one( lang_id => 'Dict::Learn::Main::Result::Language',
                     { 'foreign.language_id' => 'self.lang_id' },
