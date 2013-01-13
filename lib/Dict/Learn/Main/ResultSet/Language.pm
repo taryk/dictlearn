e892fed977f75e28be79a34b6cc6d80eda8acfaa
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-13 23:26:37 +0200

    `Language` resultset

---
+++ lib/Dict/Learn/Main/ResultSet/Language.pm
@@ -0,0 +1,28 @@
+package Dict::Learn::Main::ResultSet::Language 0.1;
+use base 'DBIx::Class::ResultSet';
+
+use namespace::autoclean;
+
+use common::sense;
+
+use Data::Printer;
+
+sub export_data {
+  my ($self) = @_;
+  my $rs = $self->search({ }, { });
+  $rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
+  $rs->all()
+}
+
+sub import_data {
+  my ($self, $data) = @_;
+  $self->populate($data);
+  return 1
+}
+
+sub clear_data {
+  my ($self) = @_;
+  $self->delete_all()
+}
+
+1;
