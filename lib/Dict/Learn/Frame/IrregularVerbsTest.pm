a14bfad03c1a63e4610a7b58c02d3c9270bff78f
Taras Iagniuk <mrtaryk@gmail.com>
2013-08-17 20:27:01 +0300

    Moosefy Dict::Learn::Frame::IrregularVerbsTest

--- lib/Dict/Learn/Frame/IrregularVerbsTest.pm
+++ lib/Dict/Learn/Frame/IrregularVerbsTest.pm
@@ -3,142 +3,358 @@ package Dict::Learn::Frame::IrregularVerbsTest 0.1;
 use Wx qw[:everything];
 use Wx::Event qw[:everything];
 
-use base 'Wx::Panel';
+use Moose;
+use MooseX::NonMoose;
+extends 'Wx::Panel';
 
-use List::Util qw[shuffle];
 use Data::Printer;
+use List::Util qw[shuffle];
 
 use Dict::Learn::Frame::IrregularVerbsTest::Result;
 
 use common::sense;
 
-use Class::XSAccessor accessors => [
-    qw| parent
-        l_position
-        l_word e_word2 e_word3
-        res res_word2 res_word3
-        btn_next btn_prev btn_reset
-        vbox hbox_words hbox_res hbox_buttons
-
-        p_current p_min p_max
-
-        words exercise total_score
-      |
-];
-
 sub TEST_ID { 0  }
 sub STEPS   { 10 }
 
-sub new {
-    my $class = shift;
-    my $self = $class->SUPER::new(splice @_ => 1);
-    $self->parent(shift);
+=item words
 
-    ### position
-    $self->l_position(
+=cut
+
+has words => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    lazy    => 1,
+    default => sub { [] },
+);
+
+=item exercise
+
+=cut
+
+has exercise => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    lazy    => 1,
+    default => sub { [] },
+);
+
+=item p_min
+
+=cut
+
+has p_min => (
+    is      => 'rw',
+    isa     => 'Int',
+    lazy    => 1,
+    default => sub { 1 },
+);
+
+=item p_max
+
+=cut
+
+has p_max => (
+    is      => 'rw',
+    isa     => 'Int',
+    lazy    => 1,
+    default => sub { STEPS },
+);
+
+=item p_current
+
+=cut
+
+has p_current => (
+    is  => 'rw',
+    isa => 'Int',
+);
+
+=item total_score
+
+=cut
+
+has total_score => (
+    is      => 'rw',
+    lazy    => 1,
+    default => sub { 0 },
+);
+
+=item parent
+
+=cut
+
+has parent => (
+    is  => 'ro',
+    isa => 'Dict::Learn::Frame',
+);
+
+=item l_position
+
+=cut
+
+has l_position => (
+    is      => 'ro',
+    isa     => 'Wx::StaticText',
+    lazy    => 1,
+    default => sub {
         Wx::StaticText->new(
-            $self,         wxID_ANY,
+            shift,         wxID_ANY,
             '1/' . STEPS,  wxDefaultPosition,
             wxDefaultSize, wxALIGN_CENTRE
         )
-    );
+    },
+);
 
-    ### words
-    $self->l_word(
+=item l_word
+
+=cut
+
+has l_word => (
+    is      => 'ro',
+    isa     => 'Wx::StaticText',
+    lazy    => 1,
+    default => sub {
         Wx::StaticText->new(
-            $self,         wxID_ANY,
+            shift,         wxID_ANY,
             'Word',        wxDefaultPosition,
             wxDefaultSize, wxALIGN_CENTRE
         )
-    );
-    $self->e_word2(
-        Wx::TextCtrl->new(
-            $self,         wxID_ANY,
-            '',            wxDefaultPosition,
-            wxDefaultSize, wxTE_LEFT
-        )
-    );
-    $self->e_word3(
-        Wx::TextCtrl->new(
-            $self,         wxID_ANY,
-            '',            wxDefaultPosition,
-            wxDefaultSize, wxTE_LEFT
-        )
-    );
+    },
+);
 
-    # layout
-    $self->hbox_words(Wx::BoxSizer->new(wxHORIZONTAL));
-    $self->hbox_words->Add($self->l_word, 0,
+=item e_word2
+
+=cut
+
+has e_word2 => (
+    is      => 'ro',
+    isa     => 'Wx::TextCtrl',
+    lazy    => 1,
+    default => sub {
+        Wx::TextCtrl->new(
+            shift,         wxID_ANY,
+            '',            wxDefaultPosition,
+            wxDefaultSize, wxTE_LEFT
+        )
+    },
+);
+
+=item e_word3
+
+=cut
+
+has e_word3 => (
+    is      => 'ro',
+    isa     => 'Wx::TextCtrl',
+    lazy    => 1,
+    default => sub {
+        Wx::TextCtrl->new(
+            shift,         wxID_ANY,
+            '',            wxDefaultPosition,
+            wxDefaultSize, wxTE_LEFT
+        )
+    },
+);
+
+=item hbox_words
+
+=cut
+
+has hbox_words => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
+
+sub _build_hbox_words {
+    my $self = shift;
+
+    my $hbox_words = Wx::BoxSizer->new(wxHORIZONTAL);
+    $hbox_words->Add($self->l_word, 0,
         wxRIGHT | wxTOP | wxLEFT | wxEXPAND, 5);
-    $self->hbox_words->Add($self->e_word2, 1, wxRIGHT | wxEXPAND, 5);
-    $self->hbox_words->Add($self->e_word3, 1, wxEXPAND,           0);
+    $hbox_words->Add($self->e_word2, 1, wxRIGHT | wxEXPAND, 5);
+    $hbox_words->Add($self->e_word3, 1, wxEXPAND,           0);
 
-    ### res
-    $self->res(
+    return $hbox_words;
+}
+
+=item res
+
+=cut
+
+has res => (
+    is      => 'ro',
+    isa     => 'Wx::StaticText',
+    lazy    => 1,
+    default => sub {
         Wx::StaticText->new(
-            $self,         wxID_ANY,
+            shift,         wxID_ANY,
             '',            wxDefaultPosition,
             wxDefaultSize, wxALIGN_CENTRE
         )
-    );
-    $self->res_word2(
+    },
+);
+
+=item res_word2
+
+=cut
+
+has res_word2 => (
+    is      => 'ro',
+    isa     => 'Wx::StaticText',
+    lazy    => 1,
+    default => sub {
         Wx::StaticText->new(
-            $self,         wxID_ANY,
+            shift,         wxID_ANY,
             '',            wxDefaultPosition,
             wxDefaultSize, wxALIGN_CENTRE
         )
-    );
-    $self->res_word3(
+    },
+);
+
+=item res_word3
+
+=cut
+
+has res_word3 => (
+    is      => 'ro',
+    isa     => 'Wx::StaticText',
+    lazy    => 1,
+    default => sub {
         Wx::StaticText->new(
-            $self,         wxID_ANY,
+            shift,         wxID_ANY,
             '',            wxDefaultPosition,
             wxDefaultSize, wxALIGN_CENTRE
         )
-    );
+    },
+);
 
-    # layout
-    $self->hbox_res(Wx::BoxSizer->new(wxHORIZONTAL));
-    $self->hbox_res->Add($self->res, 0, wxRIGHT | wxTOP | wxLEFT | wxEXPAND,
-        5);
-    $self->hbox_res->Add($self->res_word2, 1, wxRIGHT | wxEXPAND, 5);
-    $self->hbox_res->Add($self->res_word3, 1, wxEXPAND,           0);
+=item hbox_res
 
-    ### buttons
-    $self->btn_prev(
-        Wx::Button->new(
-            $self, wxID_ANY, 'Prev', wxDefaultPosition, wxDefaultSize
-        )
-    );
-    $self->btn_next(
-        Wx::Button->new(
-            $self, wxID_ANY, 'Next', wxDefaultPosition, wxDefaultSize
-        )
-    );
-    $self->btn_reset(
-        Wx::Button->new(
-            $self, wxID_ANY, 'Reset', wxDefaultPosition, wxDefaultSize
-        )
-    );
+=cut
 
-    # layout
-    $self->hbox_buttons(Wx::BoxSizer->new(wxHORIZONTAL));
-    $self->hbox_buttons->Add($self->btn_prev,  0, wxALL | wxGROW,  0);
-    $self->hbox_buttons->Add($self->btn_next,  0, wxALL | wxGROW,  0);
-    $self->hbox_buttons->Add($self->btn_reset, 0, wxLEFT | wxGROW, 40);
+has hbox_res => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
 
-    ### main layout
-    $self->vbox(Wx::BoxSizer->new(wxVERTICAL));
-    $self->vbox->Add($self->l_position,   0, wxTOP | wxGROW, 5);
-    $self->vbox->Add($self->hbox_words,   0, wxTOP | wxGROW, 20);
-    $self->vbox->Add($self->hbox_res,     0, wxTOP | wxGROW, 5);
-    $self->vbox->Add($self->hbox_buttons, 0, wxTOP | wxGROW, 20);
+sub _build_hbox_res {
+    my $self = shift;
+
+    my $hbox_res = Wx::BoxSizer->new(wxHORIZONTAL);
+    $hbox_res->Add($self->res, 0, wxRIGHT | wxTOP | wxLEFT | wxEXPAND, 5);
+    $hbox_res->Add($self->res_word2, 1, wxRIGHT | wxEXPAND, 5);
+    $hbox_res->Add($self->res_word3, 1, wxEXPAND,           0);
+
+    return $hbox_res;
+}
+
+=item btn_prev
+
+=cut
+
+has btn_prev => (
+    is      => 'ro',
+    isa     => 'Wx::Button',
+    lazy    => 1,
+    default => sub {
+        Wx::Button->new(shift, wxID_ANY, 'Prev', wxDefaultPosition,
+            wxDefaultSize)
+    },
+);
+
+=item btn_next
+
+=cut
+
+has btn_next => (
+    is      => 'ro',
+    isa     => 'Wx::Button',
+    lazy    => 1,
+    default => sub {
+        Wx::Button->new(shift, wxID_ANY, 'Next', wxDefaultPosition,
+            wxDefaultSize)
+    },
+);
+
+=item btn_reset
+
+=cut
+
+has btn_reset => (
+    is      => 'ro',
+    isa     => 'Wx::Button',
+    lazy    => 1,
+    default => sub {
+        Wx::Button->new(shift, wxID_ANY, 'Reset', wxDefaultPosition,
+            wxDefaultSize)
+    },
+);
+
+=item hbox_buttons
+
+=cut
+
+has hbox_buttons => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
+
+sub _build_hbox_buttons {
+    my $self = shift;
+
+    my $hbox_buttons = Wx::BoxSizer->new(wxHORIZONTAL);
+    $hbox_buttons->Add($self->btn_prev,  0, wxALL | wxGROW,  0);
+    $hbox_buttons->Add($self->btn_next,  0, wxALL | wxGROW,  0);
+    $hbox_buttons->Add($self->btn_reset, 0, wxLEFT | wxGROW, 40);
+
+    return $hbox_buttons;
+}
+
+=item vbox
+
+=cut
+
+has vbox => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+   );
+
+sub _build_vbox {
+    my $self = shift;
+
+    my $vbox = Wx::BoxSizer->new(wxVERTICAL);
+    $vbox->Add($self->l_position,   0, wxTOP | wxGROW, 5);
+    $vbox->Add($self->hbox_words,   0, wxTOP | wxGROW, 20);
+    $vbox->Add($self->hbox_res,     0, wxTOP | wxGROW, 5);
+    $vbox->Add($self->hbox_buttons, 0, wxTOP | wxGROW, 20);
+
+    return $vbox;
+}
+
+sub FOREIGNBUILDARGS {
+    my ($class, $parent, @args) = @_;
+
+    return @args;
+}
+
+sub BUILDARGS {
+    my ($class, $parent) = @_;
+
+    return {parent => $parent};
+}
+
+sub BUILD {
+    my ($self, @args) = @_;
+
     $self->SetSizer($self->vbox);
     $self->Layout();
     $self->vbox->Fit($self);
 
-    $self->p_min(1);
-    $self->p_max(STEPS);
-    $self->total_score(0);
     $self->init_test();
 
     # events
@@ -148,7 +364,6 @@ sub new {
     EVT_KEY_UP($self,          sub { $self->keybind($_[1]) });
     EVT_KEY_UP($self->e_word2, sub { $self->keybind2($_[1]) });
     EVT_KEY_UP($self->e_word3, sub { $self->keybind3($_[1]) });
-    $self;
 }
 
 sub keybind {
@@ -163,6 +378,7 @@ sub keybind {
 
 sub keybind2 {
     my ($self, $event) = @_;
+
     my $key = $event->GetKeyCode();
     if ($key == WXK_RETURN) {
         $self->e_word3->SetFocus();
@@ -174,6 +390,7 @@ sub keybind2 {
 
 sub keybind3 {
     my ($self, $event) = @_;
+
     my $key = $event->GetKeyCode();
     if ($event->GetKeyCode() == WXK_RETURN) {
         $self->next_word();
@@ -185,6 +402,7 @@ sub keybind3 {
 
 sub get_word {
     my ($self, $id, $n) = @_;
+
     $n //= 1;
     my $words_c = scalar @{$self->words};
     return unless $words_c > 0;
@@ -195,6 +413,7 @@ sub get_word {
 
 sub get_step {
     my ($self, $id) = @_;
+
     return $self->exercise->[$id]
         if defined $self->exercise->[$id];
 }
@@ -213,7 +432,7 @@ sub init_test {
 
     printf "Received %d verbs for test\n" => scalar @{$self->words};
 
-    for (my $id = $self->p_min - 1; $id < $self->p_max; $id++) {
+    for my $id ($self->p_min - 1 .. $self->p_max - 1) {
         my $word = $self->get_word($id);
         push @{$self->exercise} => {
             word_id => $word->{word_id},
@@ -229,6 +448,7 @@ sub init_test {
 
 sub write_step_res {
     my ($self, $id, $end) = @_;
+
     $end //= 1;
     my $ex = $self->exercise->[$id - 1];
     $ex->{end} = $end;
@@ -248,6 +468,7 @@ sub write_step_res {
 
 sub next_word {
     my ($self) = @_;
+
     $self->write_step_res($self->p_current);
     if ($self->p_current >= $self->p_max) {
         $self->result();
@@ -260,6 +481,7 @@ sub next_word {
 
 sub prev_word {
     my ($self) = @_;
+
     return unless $self->p_current > $self->p_min;
     $self->write_step_res($self->p_current, 0);
     $self->clear_fields();
@@ -270,12 +492,14 @@ sub prev_word {
 
 sub set_position {
     my ($self, $position) = @_;
+
     $self->p_current($position);
     $self->l_position->SetLabel($self->p_current . '/' . STEPS);
 }
 
 sub load_fields {
     my ($self, $en, @words) = @_;
+
     $self->l_word->SetLabel($words[0])  if $words[0];
     $self->e_word2->SetValue($words[1]) if $words[1];
     $self->e_word3->SetValue($words[2]) if $words[2];
@@ -287,6 +511,7 @@ sub load_fields {
 
 sub load_step {
     my ($self, $id) = @_;
+
     my $step = $self->get_step($id - 1);
     $self->load_fields(
         !$step->{end},       $step->{word}[0],
@@ -296,6 +521,7 @@ sub load_step {
 
 sub clear_fields {
     my ($self) = @_;
+
     $self->l_word->SetLabel('');
     $self->e_word2->Clear;
     $self->e_word3->Clear;
@@ -304,11 +530,13 @@ sub clear_fields {
 
 sub reset_test {
     my ($self) = @_;
+
     $self->init_test();
 }
 
 sub result {
     my ($self) = @_;
+
     my $result_dialog
         = Dict::Learn::Frame::IrregularVerbsTest::Result->new($self,
         wxID_ANY, 'Result', wxDefaultPosition, wxDefaultSize,
@@ -323,4 +551,6 @@ sub result {
     $self->parent->pts_irrverbs->refresh_data();
 }
 
+no Moose;
+__PACKAGE__->meta->make_immutable;
 1;
