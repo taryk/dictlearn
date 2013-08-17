88fbd1551dd6b3003fda1f3ee1c31f130a2e1b19
Taras Iagniuk <mrtaryk@gmail.com>
2013-08-17 06:57:39 +0300

    Moosefy Dict::Learn::Frame

--- lib/Dict/Learn/Frame.pm
+++ lib/Dict/Learn/Frame.pm
@@ -4,12 +4,16 @@ use Wx qw[:everything];
 use Wx::Grid;
 use Wx::Event qw[:everything];
 
-use base 'Wx::Frame';
-use common::sense;
+use Moose;
+use MooseX::NonMoose;
+extends 'Wx::Frame';
 
+use Carp qw[ croak confess ];
 use Data::Printer;
 use File::Basename 'dirname';
 use lib dirname(__FILE__) . '/../lib/';
+
+use common::sense;
 
 use Dict::Learn::Db;
 use Dict::Learn::Export;
@@ -20,133 +24,321 @@ use Dict::Learn::Frame::SearchWords;
 use Dict::Learn::Frame::IrregularVerbsTest;
 use Dict::Learn::Frame::TestSummary;
 use Dict::Learn::Frame::TranslationTest;
-
-use Class::XSAccessor accessors => [
-    qw| parent
-        vbox menu_bar menu_dicts menu_db menu_trans
-        status_bar notebook
-        p_additem p_addword p_gridwords p_search
-        pt_irrverbs
-        pts_irrverbs
-        pt_trans
-        tran
-      |
-];
+use Dict::Learn::Dictionary;
 
 sub DICT_OFFSET { 100 }
 
-sub new {
-    my ($class, $parent) = @_;
-    my $self = $class->SUPER::new(splice @_ => 1);
-    $self->parent($parent);
-    $self->SetIcon(Wx::GetWxPerlIcon());
-    $self->vbox(Wx::BoxSizer->new(wxVERTICAL));
-    $self->notebook(
-        Wx::Notebook->new(
-            $self, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0
-        )
+=item parent
+
+=cut
+
+has parent => (
+    is  => 'ro',
+);
+
+=item vbox
+
+=cut
+
+has vbox => (
+    is      => 'ro',
+    isa     => 'Wx::BoxSizer',
+    default => sub { Wx::BoxSizer->new(wxVERTICAL) },
+);
+
+{   my $menu_id=0;
+    
+    sub next_menu_id {
+        $menu_id++
+    }
+}
+
+=item menu_bar
+
+=cut
+
+has menu_bar => (
+    is         => 'ro',
+    isa        => 'Wx::MenuBar',
+    lazy_build => 1,
+);
+
+sub _build_menu_bar {
+    my ($self) = @_;
+    my $menubar = Wx::MenuBar->new(0);
+
+    my @menu = (
+        [ Dictionaries => $self->menu_dicts ],
+        [ DB => $self->menu_db ],
+        [ Translate => $self->menu_trans ], 
     );
 
-    $self->tran(Dict::Learn::Translate->new());
+    for my $menu_item (@menu) {
+        my ($caption, $menu_ref) = @$menu_item;
+        $menubar->Append($menu_ref, $caption);
+    }
+
+    $menubar
+}
+
+=item menu_dicts
+
+=cut
+
+has menu_dicts => (
+    is         => 'ro',
+    isa        => 'Wx::Menu',
+    lazy_build => 1,
+);
+
+sub _build_menu_dicts {
+    my ($self) = @_;
+    my $menu_dicts = Wx::Menu->new;
+
+    # Wx::MenuItem->new( $self->menu_dicts, wxID_ANY, "Test", "", wxITEM_NORMAL)
+    for (sort { $a->{dictionary_id} <=> $b->{dictionary_id} }
+        values %{Dict::Learn::Dictionary->all})
+    {
+        $menu_dicts->AppendRadioItem(_MENU_ID($_->{dictionary_id}),
+            $_->{dictionary_name});
+
+        # event
+        EVT_MENU($self, _MENU_ID($_->{dictionary_id}), \&dictionary_check);
+    }
+
+    $menu_dicts
+}
+
+=item menu_db
+
+=cut
+
+has menu_db => (
+    is         => 'ro',
+    isa        => 'Wx::Menu',
+    lazy_build => 1,
+);
+
+sub _build_menu_db {
+    my ($self) = @_;
+    my $menu_db = Wx::Menu->new;
+
+    my @menu = (
+        [ 'Export'             => \&db_export ],
+        [ 'Import'             => \&db_import ],
+        [ 'Clear All Data'     => \&db_clear ],
+        [ 'Clear Test Results' => \&db_clear_test_results ],
+        [ 'Analyze'            => \&analyze ],
+        [ 'Reset Analyzer'     => \&reset_analyzer ],
+    );
+
+    for my $menu_item (@menu) {
+        my ($caption, $coderef) = @$menu_item;
+        my $menu_id = $self->next_menu_id;
+        $menu_db->Append($menu_id, $caption);
+        EVT_MENU($self, $menu_id, $coderef);
+    }
+
+    $menu_db
+}
+
+=item menu_trans
+
+=cut
+
+has menu_trans => (
+    is         => 'ro',
+    isa        => 'Wx::Menu',
+    lazy_build => 1,
+);
+
+sub _build_menu_trans {
+    my ($self) = @_;
+    my $menu_trans = Wx::Menu->new;
+
+    for my $tran_backend (@{$self->tran->get_backends_list}) {
+        my $menu_id = $self->next_menu_id;
+        $menu_trans->AppendRadioItem($menu_id, $tran_backend);
+        EVT_MENU($self, $menu_id, \&set_tran_backend);
+    }
+
+    $menu_trans
+}
+
+=item status_bar
+
+=cut
+
+has status_bar => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { shift->CreateStatusBar(1, wxST_SIZEGRIP, wxID_ANY) }
+);
+
+=item notebook
+
+=cut
+
+has notebook => (
+    is      => 'ro',
+    isa     => 'Wx::Notebook',
+    lazy    => 1,
+    default => sub {
+        Wx::Notebook->new(shift, wxID_ANY,
+            wxDefaultPosition, wxDefaultSize, 0),
+    }
+);
+
+sub make_pages {
+    my ($self) = @_;
+    my @pages = (
+        [ 'Search'               => $self->p_search,     1 ],
+        [ 'Word'                 => $self->p_addword,    0 ],
+        [ 'Words'                => $self->p_gridwords,  0 ],
+        [ 'Irregular Verbs Test' => $self->pt_irrverbs,  0 ],
+        [ 'TestSummary'          => $self->pts_irrverbs, 0 ],
+        [ 'Translation Test'     => $self->pt_trans,     0 ],
+    );
+
+    for my $page_item (@pages) {
+        my ($caption, $page, $default) = @$page_item;
+        $self->notebook->AddPage($page, $caption, $default);
+    }
+}
+
+=item tran
+
+=cut
+
+has tran => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Translate',
+    lazy    => 1,
+    default => sub { Dict::Learn::Translate->new() }
+);
+
+=item p_search
+
+=cut
+
+has p_search => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::SearchWords',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::SearchWords->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    }
+);
+
+=item p_addword
+
+=cut
+
+has p_addword => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::AddWord',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::AddWord->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    }
+);
+
+=item p_gridwords
+
+=cut
+
+has p_gridwords => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::GridWords',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::GridWords->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    }
+);
+
+=item pt_irrverbs
+
+=cut
+
+has pt_irrverbs => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::IrregularVerbsTest',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::IrregularVerbsTest->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    }
+);
+
+=item pts_irrverbs
+
+=cut
+
+has pts_irrverbs => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::TestSummary',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::TestSummary->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    }
+);
+
+=item pt_trans
+
+=cut
+
+has pt_trans => (
+    is      => 'ro',
+    isa     => 'Dict::Learn::Frame::TranslationTest',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        Dict::Learn::Frame::TranslationTest->new($self, $self->notebook,
+            wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL)
+    },
+);
+
+sub FOREIGNBUILDARGS {
+    my ($class, @args) = @_;
+    return @args;
+}
+
+sub BUILDARGS {
+    my ($class, $parent) = @_;
+    return {parent => $parent};
+}
+
+sub BUILD {
+    my ($self, @args) = @_;
+
+    # FIXME: get rid of it
+    Dict::Learn::Dictionary->all;
+    
+    $self->SetIcon(Wx::GetWxPerlIcon());
+    $self->vbox->Add($self->notebook, 1, wxALL | wxEXPAND, 5);
 
     # main menu
-    my $menu_id = 0;
-    $self->menu_bar(Wx::MenuBar->new(0));
     $self->SetMenuBar($self->menu_bar);
-    $self->menu_dicts(Wx::Menu->new);
-    $self->menu_bar->Append($self->menu_dicts, 'Dictionaries');
-    $self->init_menu_dicts($self->menu_dicts);
-    $self->menu_db(Wx::Menu->new);
-    $self->menu_bar->Append($self->menu_db, 'DB');
-    $self->menu_db->Append(++$menu_id, 'Export');
-    EVT_MENU($self, $menu_id, \&db_export);
-    $self->menu_db->Append(++$menu_id, 'Import');
-    EVT_MENU($self, $menu_id, \&db_import);
-    $self->menu_db->Append(++$menu_id, 'Clear All Data');
-    EVT_MENU($self, $menu_id, \&db_clear);
-    $self->menu_db->Append(++$menu_id, 'Clear Test Results');
-    EVT_MENU($self, $menu_id, \&db_clear_test_results);
-    $self->menu_db->Append(++$menu_id, 'Analyze');
-    EVT_MENU($self, $menu_id, \&analyze);
-    $self->menu_db->Append(++$menu_id, 'Reset Analyzer');
-    EVT_MENU($self, $menu_id, \&reset_analyzer);
-    $self->menu_trans(Wx::Menu->new);
-    $self->menu_bar->Append($self->menu_trans, 'Translate');
-    $self->init_menu_translate($self->menu_trans, $menu_id);
 
-    # panel search
-
-    $self->p_search(
-        Dict::Learn::Frame::SearchWords->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-    $self->notebook->AddPage($self->p_search, 'Search', 1);
-
-    # panel addword
-
-    $self->p_addword(
-        Dict::Learn::Frame::AddWord->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-
-    $self->notebook->AddPage($self->p_addword, 'Word', 0);
-
-    # panel: table of words
-
-    $self->p_gridwords(
-        Dict::Learn::Frame::GridWords->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-    $self->notebook->AddPage($self->p_gridwords, 'Words', 0);
-
-    # Irregular Verbs test
-    $self->pt_irrverbs(
-        Dict::Learn::Frame::IrregularVerbsTest->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-    $self->notebook->AddPage($self->pt_irrverbs, 'Irregular Verbs Test', 0);
-
-    # Test Summary
-    $self->pts_irrverbs(
-        Dict::Learn::Frame::TestSummary->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-    $self->notebook->AddPage($self->pts_irrverbs, 'TestSummary', 0);
-
-    # Translation Test
-    $self->pt_trans(
-        Dict::Learn::Frame::TranslationTest->new(
-            $self,         $self->notebook,
-            wxID_ANY,      wxDefaultPosition,
-            wxDefaultSize, wxTAB_TRAVERSAL
-        )
-    );
-    $self->notebook->AddPage($self->pt_trans, 'Translation Test', 0);
+    $self->make_pages();
 
     # tell we want automatic layout
     # $self->SetAutoLayout( 1 );
-    $self->vbox->Add($self->notebook, 1, wxALL | wxEXPAND, 5);
     $self->SetSizer($self->vbox);
 
     # size the window optimally and set its minimal size
-    # $self->vbox->Fit( $self );
+    $self->Layout();
+    $self->vbox->Fit( $self );
     # $self->vbox->SetSizeHints( $self );
-    $self->status_bar($self->CreateStatusBar(1, wxST_SIZEGRIP, wxID_ANY));
 
     Dict::Learn::Dictionary->set(0);
 
@@ -156,31 +348,6 @@ sub new {
 
     # events
     EVT_CLOSE($self, \&on_close);
-    $self;
-}
-
-sub init_menu_dicts {
-    my ($self, $menu) = @_;
-
-    # Wx::MenuItem->new( $self->menu_dicts, wxID_ANY, "Test", "", wxITEM_NORMAL)
-    for (sort { $a->{dictionary_id} <=> $b->{dictionary_id} }
-        values %{Dict::Learn::Dictionary->all})
-    {
-        $menu->AppendRadioItem(_MENU_ID($_->{dictionary_id}),
-            $_->{dictionary_name});
-
-        # event
-        EVT_MENU($self, _MENU_ID($_->{dictionary_id}), \&dictionary_check);
-    }
-    $self;
-}
-
-sub init_menu_translate {
-    my ($self, $menu, $menu_id) = @_;
-    for my $tran_backend (@{$self->tran->get_backends_list}) {
-        $self->menu_trans->AppendRadioItem(++$menu_id, $tran_backend);
-        EVT_MENU($self, $menu_id, \&set_tran_backend);
-    }
 }
 
 sub dictionary_check {
@@ -279,4 +446,6 @@ sub reset_analyzer {
     $main::ioc->lookup('db')->reset_analyzer();
 }
 
+no Moose;
+__PACKAGE__->meta->make_immutable;
 1;
