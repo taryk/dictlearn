a55706afe6e63905a34ba1013feca19c4b5006e3
Taras Iagniuk <mrtaryk@gmail.com>
2013-08-18 07:48:33 +0300

    Basic gui for TestEditor

--- lib/Dict/Learn/Frame/TestEditor.pm
+++ lib/Dict/Learn/Frame/TestEditor.pm
@@ -15,6 +15,142 @@ use common::sense;
 
 sub TEST_ID { 1 }
 
+=item parent
+
+=cut
+
+has parent => (
+    is  => 'ro',
+    isa => 'Dict::Learn::Frame',
+);
+
+=item test_groups
+
+=cut
+
+has test_groups => (
+    is         => 'ro',
+    isa        => 'Wx::ListCtrl',
+    lazy_build => 1,
+);
+
+sub _build_test_groups {
+    my $self = shift;
+
+    my $test_groups = Wx::ListCtrl->new($self, wxID_ANY, wxDefaultPosition,
+        wxDefaultSize, wxLC_REPORT | wxLC_HRULES | wxLC_VRULES);
+
+    return $test_groups;
+}
+
+=item test_words
+
+=cut
+
+has test_words => (
+    is         => 'ro',
+    isa        => 'Wx::ListCtrl',
+    lazy_build => 1,
+   );
+
+sub _build_test_words {
+    my $self = shift;
+    
+    my $test_words = Wx::ListCtrl->new($self, wxID_ANY, wxDefaultPosition,
+        wxDefaultSize, wxLC_REPORT | wxLC_HRULES | wxLC_VRULES);
+    
+    return $test_words;
+}
+
+=item btn_move_left
+
+=cut
+
+has btn_move_left => (
+    is      => 'ro',
+    isa     => 'Wx::Button',
+    lazy    => 1,
+    default => sub {
+        Wx::Button->new(shift, wxID_ANY, '<- Add', wxDefaultPosition,
+            wxDefaultSize)
+    },
+);
+
+=item btn_move_right
+
+=cut
+
+has btn_move_right => (
+    is      => 'ro',
+    isa     => 'Wx::Button',
+    lazy    => 1,
+    default => sub {
+        Wx::Button->new(shift, wxID_ANY, '-> Remove', wxDefaultPosition,
+            wxDefaultSize)
+    },
+);
+
+=item vbox_btn
+
+=cut
+
+has vbox_btn => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
+
+sub _build_vbox_btn {
+    my $self = shift;
+
+    my $vbox_btn = Wx::BoxSizer->new(wxVERTICAL);
+    $vbox_btn->Add($self->btn_move_left,  0, wxLEFT, 5);
+    $vbox_btn->Add($self->btn_move_right, 0, wxLEFT, 5);
+
+    return $vbox_btn;
+}
+
+=item word_list
+
+=cut
+
+has word_list => (
+    is         => 'ro',
+    isa        => 'Wx::ListCtrl',
+    lazy_build => 1,
+);
+
+sub _build_word_list {
+    my $self = shift;
+    
+    my $word_list = Wx::ListCtrl->new($self, wxID_ANY, wxDefaultPosition,
+        wxDefaultSize, wxLC_REPORT | wxLC_HRULES | wxLC_VRULES);
+    
+    return $word_list;
+}
+
+=item hbox
+
+=cut
+
+has hbox => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
+
+sub _build_hbox {
+    my $self = shift;
+
+    my $hbox = Wx::BoxSizer->new(wxHORIZONTAL);
+    $hbox->Add($self->test_groups, 1,          wxEXPAND, 0  );
+    $hbox->Add($self->test_words,  1, wxLEFT | wxEXPAND, 5  );
+    $hbox->Add($self->vbox_btn,    0, wxTOP,             25 );
+    $hbox->Add($self->word_list,   1, wxLEFT | wxEXPAND, 5  );
+
+    return $hbox;
+}
+
 sub FOREIGNBUILDARGS {
     my ($class, $parent, @args) = @_;
 
@@ -23,11 +159,17 @@ sub FOREIGNBUILDARGS {
 
 sub BUILDARGS {
     my ($class, $parent) = @_;
+
     return { parent => $parent };
 }
 
 sub BUILD {
     my ($self, @args) = @_;
+ 
+    # layout
+    $self->SetSizer($self->hbox);
+    $self->hbox->Fit($self);
+    $self->Layout();
 
     Dict::Learn::Dictionary->cb(
         sub {
@@ -42,7 +184,6 @@ sub init {
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
-
 1;
 __END__
 
