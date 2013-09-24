d2135f8f906f7dc3c32a17e16f1e5477e1689d9c
Taras Iagniuk <mrtaryk@gmail.com>
2013-09-24 10:41:14 +0300

    Moosify Dict::Learn::Combo::WordList

--- lib/Dict/Learn/Combo/WordList.pm
+++ lib/Dict/Learn/Combo/WordList.pm
@@ -1,100 +1,137 @@
 package Dict::Learn::Combo::WordList 0.1;
-use base qw[ Wx::PlComboPopup ];
 
 use Wx qw[:everything];
 use Wx::Event qw[:everything];
 
-use Data::Printer;
+use Moose;
+extends 'Wx::PlComboPopup';
 
 use common::sense;
 
-use Class::XSAccessor accessors => [qw| parent vbox panel search lb_words |];
+use Data::Printer;
 
-sub Init {
+=item parent
+
+=cut
+
+has parent => (
+    is  => 'rw',
+    isa => 'Wx::PopupTransientWindow',
+);
+
+=item vbox
+
+=cut
+
+has vbox => (
+    is         => 'ro',
+    isa        => 'Wx::BoxSizer',
+    lazy_build => 1,
+);
+
+sub _build_vbox {
     my $self = shift;
-    $self->{item_index}  = undef;
-    $self->{cb}          = undef;
-    $self->{initialized} = undef;
-    $self;
+
+    my $vbox = Wx::BoxSizer->new(wxVERTICAL);
+    $vbox->Add($self->lb_words, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT, 2);
+
+    return $vbox;
 }
+
+=item panel
+
+=cut
+
+has panel => (
+    is         => 'ro',
+    isa        => 'Wx::Panel',
+    lazy_build => 1,
+);
+
+sub _build_panel {
+    my $self = shift;
+
+    my $panel = Wx::Panel->new($self->parent, wxID_ANY,
+        wxDefaultPosition, wxDefaultSize, wxCAPTION, 'words');
+
+    return $panel;
+}
+
+=item lb_words
+
+=cut
+
+has lb_words => (
+    is         => 'ro',
+    isa        => 'Wx::ListCtrl',
+    lazy_build => 1,
+);
+
+sub _build_lb_words {
+    my $self = shift;
+
+    my $lb_words
+        = Wx::ListCtrl->new($self->panel, wxID_ANY,
+        wxDefaultPosition, wxDefaultSize,
+        wxLC_REPORT | wxLC_HRULES | wxLC_VRULES);
+
+    $lb_words->InsertColumn(0, 'id',   wxLIST_FORMAT_LEFT, 35);
+    $lb_words->InsertColumn(1, 'word', wxLIST_FORMAT_LEFT, 200);
+
+    EVT_LIST_ITEM_ACTIVATED($self->parent, $lb_words,
+        sub { $self->on_select(@_) });
+
+    return $lb_words;
+}
+
+=item initialized
+
+=cut
+
+has initialized => (
+    is  => 'rw',
+    isa => 'Bool',
+);
 
 sub Create {
     my ($self, $parent) = @_;
+
     $self->parent($parent);
-
-    # widgets
-    $self->panel(
-        Wx::Panel->new(
-            $parent,           wxID_ANY,
-            wxDefaultPosition, wxDefaultSize,
-            wxCAPTION,         'words'
-        )
-    );
-
-    # $self->search( Wx::TextCtrl->new( $self->panel, wxID_ANY, 'test', wxDefaultPosition, wxDefaultSize ) );
-    # $self->search->SetEditable(1);
-    $self->lb_words(
-        Wx::ListCtrl->new(
-            $self->panel,      wxID_ANY,
-            wxDefaultPosition, wxDefaultSize,
-            wxLC_REPORT | wxLC_HRULES | wxLC_VRULES
-        )
-    );
-    $self->lb_words->InsertColumn(0, 'id',   wxLIST_FORMAT_LEFT, 35);
-    $self->lb_words->InsertColumn(1, 'word', wxLIST_FORMAT_LEFT, 200);
-
-    # layout
-    $self->vbox(Wx::BoxSizer->new(wxVERTICAL));
-
-    # $self->vbox->Add( $self->search, 0, wxEXPAND|wxTOP|wxLEFT|wxRIGHT, 2 );
-    $self->vbox->Add($self->lb_words, 1, wxEXPAND | wxTOP | wxLEFT | wxRIGHT,
-        2);
-
-    # main
     $self->panel->SetSizer($self->vbox);
     $self->panel->Layout();
     $self->vbox->Fit($self->panel);
-    EVT_LIST_ITEM_ACTIVATED($parent, $self->lb_words,
-        sub { $self->on_select(@_) });
 
-    $self->panel;
+    return 1;
 }
 
 sub GetControl {
     my $self = shift;
 
-    # p($self);
-    # say "GetControl";
     $self->panel;
 }
 
 sub OnPopup {
     my $self = shift;
+
     $self->SUPER::OnPopup();
 
-    unless ($self->{initialized}) {
+    unless ($self->initialized) {
         $self->initialize_words();
-        $self->{initialized} = 1;
+        $self->initialized(1);
     }
-
     # Wx::LogMessage( "Popping up" );
 }
 
 sub OnDismiss {
     my $self = shift;
+
     $self->SUPER::OnDismiss();
-
     # Wx::LogMessage( "Being dismissed" );
-}
-
-sub SetStringValue {
-    my ($self, $string) = @_;
-
-    # p($string);
 }
 
 sub GetStringValue {
     my ($self) = @_;
+
     my $item_id = $self->lb_words->GetNextItem(-1, wxLIST_NEXT_ALL,
         wxLIST_STATE_SELECTED);
     $self->parent->GetParent()
@@ -102,24 +139,18 @@ sub GetStringValue {
     $self->lb_words->GetItem($item_id, 1)->GetText;
 }
 
-sub cb {
-    my ($self, $cb) = @_;
-    return unless ref $cb eq 'CODE';
-    $self->{cb} = $cb;
-}
-
 sub on_select {
     my ($self, $parent, $event) = @_;
 
-    # p($event->GetIndex);
+    # $event->GetIndex
     my $text = $self->lb_words->GetItem($event->GetIndex, 1)->GetText;
 
-    # p($text);
     $self->Dismiss();
 }
 
 sub initialize_words {
     my $self = shift;
+
     $self->lb_words->DeleteAllItems();
     my @words
         = $main::ioc->lookup('db')->schema->resultset('Word')
@@ -132,4 +163,6 @@ sub initialize_words {
     }
 }
 
+no Moose;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
