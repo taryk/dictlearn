a35fed95026ba4023d6d813e5c97e9a3f661bc76
Taras Iagniuk <mrtaryk@gmail.com>
2013-09-26 03:48:24 +0300

    Rename table: wordclass => partofspeech Rename columns: wordclass_id => partofspeech_id

--- db/dictlearn.sql
+++ db/dictlearn.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS word (
   `word2`             VARCHAR  NULL,
   `word3`             VARCHAR  NULL,
   `lang_id`           INTEGER  NOT NULL DEFAULT 0,
-  `wordclass_id`      INTEGER  NOT NULL DEFAULT 0,
+  `partofspeech_id`   INTEGER  NOT NULL DEFAULT 0,
   `irregular`         INTEGER  NULL,
   `in_test`           INTEGER  NULL DEFAULT 1,
   `example`           INTEGER  NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS word (
 CREATE TABLE IF NOT EXISTS word_xref (
   `word1_id`          INTEGER  NOT NULL DEFAULT 0,
   `word2_id`          INTEGER  NOT NULL DEFAULT 0,
-  `wordclass_id`      INTEGER  NOT NULL DEFAULT 0,
+  `partofspeech_id`   INTEGER  NOT NULL DEFAULT 0,
   `dictionary_id`     INTEGER  NOT NULL DEFAULT 0,
   `rel_type`          INTEGER  NOT NULL DEFAULT 0,
   `category_id`       INTEGER  NOT NULL DEFAULT 0,
@@ -48,14 +48,14 @@ CREATE TABLE IF NOT EXISTS example_xref (
 CREATE TABLE IF NOT EXISTS word_example_xref (
   `word_id`           INTEGER  NOT NULL DEFAULT 0,
   `example_id`        INTEGER  NOT NULL DEFAULT 0,
-  `wordclass_id`      INTEGER  NOT NULL DEFAULT 0,
+  `partofspeech_id`   INTEGER  NOT NULL DEFAULT 0,
   `note`              TEXT,
   `cdate`             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mdate`             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS wordclass (
-  `wordclass_id`      INTEGER  PRIMARY KEY ASC AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS partofspeech (
+  `partofspeech_id`   INTEGER  PRIMARY KEY ASC AUTOINCREMENT,
   `name_orig`         VARCHAR  NOT NULL DEFAULT '<none>',
   `name_tr`           VARCHAR  NOT NULL DEFAULT '<none>',
   `abbr`              VARCHAR  NOT NULL DEFAULT '<none>',
@@ -123,7 +123,7 @@ CREATE TABLE IF NOT EXISTS test_category (
 CREATE TABLE IF NOT EXISTS test_category_word_xref (
   `test_category_id`     INTEGER  NOT NULL DEFAULT 0,
   `word_id`              INTEGER  NOT NULL DEFAULT 0,
-  `wordclass_id`         INTEGER  NOT NULL DEFAULT 0,
+  `partofspeech_id`         INTEGER  NOT NULL DEFAULT 0,
   `cdate`                DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mdate`                DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP  
 );
@@ -134,24 +134,24 @@ INSERT INTO language (language_id, language_name) VALUES (1, 'Ukrainian');
 INSERT INTO dictionary (dictionary_id, dictionary_name, language_orig_id, language_tr_id) VALUES (0, 'English-Ukrainian', 0, 1);
 INSERT INTO dictionary (dictionary_id, dictionary_name, language_orig_id, language_tr_id) VALUES (1, 'Ukrainian-English', 1, 0);
 
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (0, 'Noun', 'Іменник','n');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (1, 'Verb', 'Дієслово','v');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (2, 'Participle', 'Дієприкметник', 'p');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (3, 'Interjection', 'Вигук', 'i');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (4, 'Pronoun', 'Займенник', 'pro');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (5, 'Preposition', 'Прийменник', 'pre');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (6, 'Adverb', 'Прислівник', 'adv');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (7, 'Conjunction', 'Сполучник', 'c');
-INSERT INTO wordclass (wordclass_id, name_orig, name_tr, abbr) VALUES (8, 'Adjective', 'Прикметник', 'adj');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (0, 'Noun', 'Іменник','n');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (1, 'Verb', 'Дієслово','v');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (2, 'Participle', 'Дієприкметник', 'p');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (3, 'Interjection', 'Вигук', 'i');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (4, 'Pronoun', 'Займенник', 'pro');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (5, 'Preposition', 'Прийменник', 'pre');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (6, 'Adverb', 'Прислівник', 'adv');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (7, 'Conjunction', 'Сполучник', 'c');
+INSERT INTO partofspeech (partofspeech_id, name_orig, name_tr, abbr) VALUES (8, 'Adjective', 'Прикметник', 'adj');
 
-INSERT INTO word (word_id, word, lang_id, wordclass_id) VALUES (0, 'test', 0, 0);
-INSERT INTO word (word_id, word, lang_id, wordclass_id) VALUES (1, 'тест', 1, 0);
-INSERT INTO word (word_id, word, lang_id, wordclass_id) VALUES (2, 'протестуй', 1, 1);
+INSERT INTO word (word_id, word, lang_id, partofspeech_id) VALUES (0, 'test', 0, 0);
+INSERT INTO word (word_id, word, lang_id, partofspeech_id) VALUES (1, 'тест', 1, 0);
+INSERT INTO word (word_id, word, lang_id, partofspeech_id) VALUES (2, 'протестуй', 1, 1);
 
-INSERT INTO word_xref (word1_id, word2_id, dictionary_id, wordclass_id) VALUES (0, 1, 0, 0);
-INSERT INTO word_xref (word1_id, word2_id, dictionary_id, wordclass_id) VALUES (1, 0, 1, 0);
-INSERT INTO word_xref (word1_id, word2_id, dictionary_id, wordclass_id) VALUES (0, 2, 0, 1);
-INSERT INTO word_xref (word1_id, word2_id, dictionary_id, wordclass_id) VALUES (2, 0, 1, 1);
+INSERT INTO word_xref (word1_id, word2_id, dictionary_id, partofspeech_id) VALUES (0, 1, 0, 0);
+INSERT INTO word_xref (word1_id, word2_id, dictionary_id, partofspeech_id) VALUES (1, 0, 1, 0);
+INSERT INTO word_xref (word1_id, word2_id, dictionary_id, partofspeech_id) VALUES (0, 2, 0, 1);
+INSERT INTO word_xref (word1_id, word2_id, dictionary_id, partofspeech_id) VALUES (2, 0, 1, 1);
 
 INSERT INTO example (example_id, example, lang_id) VALUES (0, 'This is a test', 0);
 INSERT INTO example (example_id, example, lang_id) VALUES (1, 'Це є тест', 1);
