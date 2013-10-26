a1d8eea7ff3b93677e10e7cbc036c8997d2229bb
Taras Iagniuk <mrtaryk@gmail.com>
2013-10-26 22:58:33 +0300

    Fix TestCategory relations

--- lib/Dict/Learn/Main/Result/TestCategory.pm
+++ lib/Dict/Learn/Main/Result/TestCategory.pm
@@ -6,16 +6,18 @@ __PACKAGE__->table('test_category');
 __PACKAGE__->add_columns(qw[ test_category_id test_id dictionary_id name cdate mdate ]);
 __PACKAGE__->set_primary_key('test_category_id');
 __PACKAGE__->has_one(
-    lang_id => 'Dict::Learn::Main::Result::Test',
-    {'foreign.test_id' => 'self.test_id'},
-    {   cascade_delete => 0,
+    test => 'Dict::Learn::Main::Result::Test',
+    { 'foreign.test_id' => 'self.test_id' },
+    {
+        cascade_delete => 0,
         cascade_update => 0
     }
 );
 __PACKAGE__->has_one(
-    sessions => 'Dict::Learn::Main::Result::Dictionary',
-    {'foreign.dictionary_id' => 'self.dictionary_id'},
-    {   cascade_delete => 0,
+    dictionary => 'Dict::Learn::Main::Result::Dictionary',
+    { 'foreign.dictionary_id' => 'self.dictionary_id' },
+    {
+        cascade_delete => 0,
         cascade_update => 0
     }
 );
