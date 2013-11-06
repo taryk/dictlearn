afc2973364afec04af3bb506357b01dc984366d4
Taras Iagniuk <mrtaryk@gmail.com>
2013-11-06 03:19:27 +0200

    Add `Container` package

---
+++ lib/Container.pm
@@ -0,0 +1,51 @@
+package Container;
+
+use IOC::Slinky::Container;
+
+use common::sense;
+
+{
+    my ($ioc, %params);
+
+    sub params { shift; %params = @_ }
+
+    sub ioc {
+        return $ioc //= IOC::Slinky::Container->new(
+            config => {
+                container => {
+                    schema => {
+                        _class            => 'Dict::Learn::Main',
+                        _constructor      => 'connect',
+                        _constructor_args => [
+                            "dbi:SQLite:$params{dbfile}",
+                            '', '',
+                            {   sqlite_unicode => 1,
+                                loader_options => {
+                                    debug => $params{debug},
+                                    use_namespaces => 1
+                                }
+                            }
+                        ]
+                    },
+                    db => {
+                        _class            => 'Dict::Learn::Db',
+                        _constructor      => 'new',
+                        _constructor_args => [
+                            schema => { _ref => 'schema' },
+                        ],
+                    },
+                    dictionary => {
+                        _class            => 'Dict::Learn::Dictionary',
+                        _constructor      => 'new',
+                        _constructor_args => [{_ref => 'db'},]
+                    },
+
+                },
+            }
+        );
+    }
+
+    sub lookup { shift; ioc->lookup(@_) }
+};
+
+1;
