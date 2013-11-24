0f36230c3a4dfea9fa5a6f811eeab1ea67b99808
Taras Iagniuk <mrtaryk@gmail.com>
2013-11-24 03:37:06 +0200

    Don't need the closure here

--- lib/Container.pm
+++ lib/Container.pm
@@ -4,48 +4,46 @@ use IOC::Slinky::Container;
 
 use common::sense;
 
-{
-    my ($ioc, %params);
+my %params;
 
-    sub params { shift; %params = @_ }
+sub params { shift; %params = @_ }
 
-    sub ioc {
-        return $ioc //= IOC::Slinky::Container->new(
-            config => {
-                container => {
-                    schema => {
-                        _class            => 'Dict::Learn::Main',
-                        _constructor      => 'connect',
-                        _constructor_args => [
-                            "dbi:SQLite:$params{dbfile}",
-                            '', '',
-                            {   sqlite_unicode => 1,
-                                loader_options => {
-                                    debug => $params{debug},
-                                    use_namespaces => 1
-                                }
+sub ioc {
+    return state $ioc //= IOC::Slinky::Container->new(
+        config => {
+            container => {
+                schema => {
+                    _class            => 'Dict::Learn::Main',
+                    _constructor      => 'connect',
+                    _constructor_args => [
+                        "dbi:SQLite:$params{dbfile}",
+                        '', '',
+                        {   sqlite_unicode => 1,
+                            loader_options => {
+                                debug => $params{debug},
+                                use_namespaces => 1
                             }
-                        ]
-                    },
-                    db => {
-                        _class            => 'Dict::Learn::Db',
-                        _constructor      => 'new',
-                        _constructor_args => [
-                            schema => { _ref => 'schema' },
-                        ],
-                    },
-                    dictionary => {
-                        _class            => 'Dict::Learn::Dictionary',
-                        _constructor      => 'new',
-                        _constructor_args => [{_ref => 'db'},]
-                    },
-
+                        }
+                    ]
                 },
-            }
-        );
-    }
+                db => {
+                    _class            => 'Dict::Learn::Db',
+                    _constructor      => 'new',
+                    _constructor_args => [
+                        schema => { _ref => 'schema' },
+                    ],
+                },
+                dictionary => {
+                    _class            => 'Dict::Learn::Dictionary',
+                    _constructor      => 'new',
+                    _constructor_args => [{_ref => 'db'},]
+                },
 
-    sub lookup { shift; ioc->lookup(@_) }
-};
+            },
+        }
+    );
+}
+
+sub lookup { shift; ioc->lookup(@_) }
 
 1;
