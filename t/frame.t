899568b9846e0485984927c08d7ada33a5b48e48
Taras Iagniuk <mrtaryk@gmail.com>
2013-12-07 14:09:00 +0200

    Initial version of a test for Dict::Learn::Frame::TestEditor

--- t/frame.t
+++ t/frame.t
@@ -2,5 +2,6 @@
 use lib::abs 'lib';
 use Test::Dict::Learn::Frame::AddWord;
 use Test::Dict::Learn::Frame::SearchWords;
+use Test::Dict::Learn::Frame::TestEditor;
 
 Test::Class->runtests;
---
+++ t/lib/Test/Dict/Learn/Frame/TestEditor.pm
@@ -0,0 +1,61 @@
+package Test::Dict::Learn::Frame::TestEditor;
+
+use parent 'Test::Dict::Learn::Frame::Base';
+use common::sense;
+
+use Test::More;
+use Wx qw[:everything];
+
+use lib::abs qw( ../../../../../../lib );
+
+use Dict::Learn::Frame::TestEditor;
+
+sub startup : Test(startup => no_plan) {
+    my ($self) = @_;
+
+    my $parent = bless {} => 'Dict::Learn::Frame';
+
+    # `Wx::Panel` wants parent frame to be real
+    my $frame = Wx::Frame->new(undef, wxID_ANY, 'Test');
+
+    $self->{frame}
+        = Dict::Learn::Frame::TestEditor->new($parent, $frame, wxID_ANY,
+        wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
+    *Dict::Learn::Frame::TestEditor::set_status_text = sub { };
+
+    $self->SUPER::startup();
+
+    ok($self->{frame}, qw{TestEditor page created});
+}
+
+sub fields : Tests {
+    my ($self) = @_;
+
+    for (
+        [qw(parent)                           => 'Dict::Learn::Frame'],
+        [qw(cb_lookup)                        => 'Wx::ComboBox'],
+        [qw(partofspeech)                     => 'HashRef'],
+        [qw(test_groups test_words word_list) => 'Wx::ListCtrl'],
+        [
+            qw(
+                hbox_test_groups vbox_test_groups
+                vbox_btn vbox_word_list
+                hbox
+              ) => 'Wx::BoxSizer'
+        ],
+        [
+            qw(
+                btn_add_group btn_del_group btn_update_group
+                btn_move_left btn_move_right btn_reload
+              ) => 'Wx::Button'
+        ],
+        )
+    {
+        my $type = pop @$_;
+        $self->test_field(name => $_, type => $type, is => 'ro') for @$_;
+    }
+
+}
+
+1;
