8c19a682df79d0f1b9223e6b2c8b4dc1badc885d
Taras Iagniuk <mrtaryk@gmail.com>
2013-12-25 03:41:25 +0200

    Add an initial version of a test for Dict::Learn::Widget::Sidebar

---
+++ t/lib/Test/Dict/Learn/Widget/Sidebar.pm
@@ -0,0 +1,50 @@
+package Test::Dict::Learn::Widget::Sidebar;
+
+use parent 'Test::Dict::Learn::Frame::Base';
+use common::sense;
+
+use Test::More;
+use Wx qw[:everything];
+
+use lib::abs qw( ../../../../../../lib );
+
+use Dict::Learn::Widget::Sidebar;
+
+sub startup : Test(startup => no_plan) {
+    my ($self) = @_;
+
+    # `Wx::Panel` wants parent frame to be real
+    my $parent = Wx::Frame->new(undef, wxID_ANY, 'parent');
+
+    $self->{frame}
+        = Dict::Learn::Widget::Sidebar->new($parent, wxID_ANY,
+        wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
+    $self->SUPER::startup();
+
+    ok($self->{frame}, qw{Sidebar panel created});
+}
+
+sub fields : Tests {
+    my ($self) = @_;
+
+    for (
+        [qw(parent)            => 'Wx::Window'],
+        [qw(html)              => 'Wx::HtmlWindow'],
+        [qw(hbox_buttons vbox) => 'Wx::BoxSizer'],
+        [
+            qw(btn_edit_word btn_unlink_word btn_delete_word btn_refresh) =>
+                'Wx::Button'
+        ],
+        )
+    {
+        my $type = pop @$_;
+        $self->test_field(name => $_, type => $type, is => 'ro') for @$_;
+    }
+    for ([qw(word_id) => 'Int']) {
+        my $type = pop @$_;
+        $self->test_field(name => $_, type => $type, is => 'rw') for @$_;
+    }
+}
+
+1;
--- t/widget.t
+++ t/widget.t
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 use lib::abs 'lib';
 use Test::Dict::Learn::Widget::LookupPhrases;
+use Test::Dict::Learn::Widget::Sidebar;
 
 Test::Class->runtests;
