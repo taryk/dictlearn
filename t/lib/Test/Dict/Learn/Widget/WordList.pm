d38ff55ba7631fd6b01a6f05a020db44b2045f73
Taras Iagniuk <mrtaryk@gmail.com>
2013-12-30 01:40:34 +0200

    Add an initial version of the test for Dict::Learn::Widget::WordList

---
+++ t/lib/Test/Dict/Learn/Widget/WordList.pm
@@ -0,0 +1,45 @@
+package Test::Dict::Learn::Widget::WordList;
+
+use parent 'Test::Dict::Learn::Frame::Base';
+use common::sense;
+
+use Test::More;
+use Wx qw[:everything];
+use Data::Printer;
+
+use lib::abs qw( ../../../../../../lib );
+
+use Dict::Learn::Widget::WordList;
+
+sub startup : Test(startup => no_plan) {
+    my ($self) = @_;
+
+    # Construct a Dict::Learn::Widget::WordList instance
+    $self->{frame} = bless {} => 'Dict::Learn::Widget::WordList';
+    $self->{frame}->parent(Wx::PopupTransientWindow->new(undef, wxID_ANY));
+
+    $self->SUPER::startup();
+
+    ok($self->{frame}, qw{WordList panel created});
+}
+
+sub fields : Tests {
+    my ($self) = @_;
+
+    for (
+        [qw(parent)   => 'Wx::PopupTransientWindow'],
+        [qw(panel)    => 'Wx::Panel'],
+        [qw(vbox)     => 'Wx::BoxSizer'],
+        [qw(lb_words) => 'Wx::ListCtrl'],
+        )
+    {
+        my $type = pop @$_;
+        $self->test_field(name => $_, type => $type, is => 'ro') for @$_;
+    }
+    for ([qw(initialized) => 'Bool']) {
+        my $type = pop @$_;
+        $self->test_field(name => $_, type => $type, is => 'rw') for @$_;
+    }
+}
+
+1;
--- t/widget.t
+++ t/widget.t
@@ -2,5 +2,6 @@
 use lib::abs 'lib';
 use Test::Dict::Learn::Widget::LookupPhrases;
 use Test::Dict::Learn::Widget::Sidebar;
+use Test::Dict::Learn::Widget::WordList;
 
 Test::Class->runtests;
